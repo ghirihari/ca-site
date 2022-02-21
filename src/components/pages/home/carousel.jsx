9d8e36f1468a9495946ad4b61dc2ff8a56d14917
Ghiri <ghirihari@gmail.com>
2022-02-21 09:15:59 +0530

    Mobile Res Carousel

--- src/components/pages/home/carousel.jsx
+++ src/components/pages/home/carousel.jsx
@@ -7,7 +7,7 @@ const Carousel = () => {
     const rightButtonIcon = <svg className="feather feather-chevron-right CarouselIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>;
     
     return (
-        <div className="carousel-container">
+        <div className="carousel-container d-none d-lg-block">
             <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -6,16 +6,14 @@ const Home = () => {
   return (
     <div>
       <Carousel/>
-      <div className='container'>
       <div className="bannerBG">
-            <div className="banner">
+            <div className="banner container">
                 <h4 className="bannerText">A peer reviewed audit firm in Coimbatore with 43 years of experience</h4>
                 <button className="btn btn-warning">Fix an appointment
                     <svg className="CarouselIcon feather feather-chevron-right" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>
                 </button>
             </div>
         </div>
-      </div>
       <Content/>
     </div>
   )
