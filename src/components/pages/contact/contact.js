b968771bac756f25d3443145b8e908f10150280d
Ghiri <ghirihari@gmail.com>
2022-03-13 00:08:56 +0530

    done: email.js

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -1,56 +1,79 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import './contact.css'
 import {ImLocation2} from 'react-icons/im'
 import {GiRotaryPhone} from 'react-icons/gi'
 import {IoIosMail} from 'react-icons/io'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return (
-    <div className="expContainer">
-        <div className='contentTitle'>
-            <label className="subtitle">Get in touch</label>
-            <hr className='line'/>
-        </div>
-        <div className='container'>
-            <div className='expContainer' style={{margin:'20px 0px'}}>
-                <div className='row'>
-                    <div className='col d-none d-lg-block' style={{display:'flex', alignItems:'center'}}>
-                        <div className="contactCard">
-                            <h2>J Vishnu & Associates</h2>
-                            <div>
-                                <ImLocation2 size={32}/>
+    const form = useRef();
+    const [button, setButton] = useState({name:'Send', class:'btn-warning'})
+    const sendMail = (e) => {
+        e.preventDefault();
+        setButton({name:'Sending', class:'btn-warning'})
+
+        emailjs.sendForm('service_8lmyc6c', 'template_ij9tpss', form.current, 'user_L0Y9hEo4STMqYeIfDSDrB')
+            .then((result) => {
+                console.log(result.text);
+                setButton({name:'Message Sent', class:'btn-success'})
+            }, (error) => {
+                setButton({name:'Failed, Try again later', class:'btn-danger'})
+            });
+    }
+    
+
+    return (
+        <div className="expContainer">
+            <div className='contentTitle'>
+                <label className="subtitle">Get in touch</label>
+                <hr className='line'/>
+            </div>
+            <div className='container'>
+                <div className='expContainer' style={{margin:'20px 0px'}}>
+                    <div className='row'>
+                        <div className='col d-none d-lg-block' style={{display:'flex', alignItems:'center'}}>
+                            <div className="contactCard">
+                                <h2>J Vishnu & Associates</h2>
                                 <div>
-                                    <a
-                                        target="_blank" rel="noopener noreferrer"
-                                        href='https://www.google.com/maps/place/A+Jayachandran+%26+Associates.+CHARTERED+ACCOUNTANTS/@11.0085815,76.9515132,17z/data=!4m5!3m4!1s0x3ba8598a9559b763:0x5edb60c1ee0872d!8m2!3d11.0085762!4d76.9537019'
-                                        className='locationLink'>
-                                            23-4 Sambandam road east, RS Puram, Coimbatore-641002
-                                    </a>
+                                    <ImLocation2 size={32}/>
+                                    <div>
+                                        <a
+                                            target="_blank" rel="noopener noreferrer"
+                                            href='https://www.google.com/maps/place/A+Jayachandran+%26+Associates.+CHARTERED+ACCOUNTANTS/@11.0085815,76.9515132,17z/data=!4m5!3m4!1s0x3ba8598a9559b763:0x5edb60c1ee0872d!8m2!3d11.0085762!4d76.9537019'
+                                            className='locationLink'>
+                                                23-4 Sambandam road east, RS Puram, Coimbatore-641002
+                                        </a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <IoIosMail size={32}/> 
+                                    <label>cajvishnu@gmail.com</label>
+                                </div>
+                                <div>
+                                    <GiRotaryPhone size={32}/>
+                                    <label>0422-4952586 / 0422-2474786</label>
                                 </div>
                             </div>
-                            <div>
-                                <IoIosMail size={32}/> 
-                                <label>cajvishnu@gmail.com</label>
-                            </div>
-                            <div>
-                                <GiRotaryPhone size={32}/>
-                                <label>0422-4952586 / 0422-2474786</label>
-                            </div>
                         </div>
-                    </div>
-                    <div className='col'>
-                        <h4>Leave us a message</h4>
-                        <div className="contactForm">
-                            <input type="email" className="form-control" placeholder="Name"/>
-                            <input type="email" className="form-control" placeholder="Email"/>
-                            <textarea type="email" className="form-control" placeholder="Enter your message"/>
-                            <button className='btn btn-warning form-control'>Submit</button>
+                        <div className='col'>
+                            <h4>Leave us a message</h4>
+                            <form className="contactForm" ref={form} onSubmit={sendMail}>
+                                <input type="text" name="name" className="form-control" placeholder="Name"/>
+                                <input type="email" name="email" className="form-control" placeholder="Email"/>
+                                <textarea name="message" className="form-control" placeholder="Enter your message"/>
+                                <button 
+                                    type="submit" 
+                                    className={'btn form-control '+button.class}
+                                    disabled={button.class==="btn-success"}
+                                >
+                                    {button.name}
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
   )
 }
 
