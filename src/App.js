97691e2ad54ce3a0eb9297ed1136a5e847693fe9
Ghiri Harihaaran <ghiribs.harihaaran@striim.com>
2024-09-01 01:39:22 +0530

    build

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import React from "react";
 import "./Styles.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/header/navBar";
-import InfoBar from "./components/header/infoBar";
 import Home from "./components/pages/home/home";
-import About from "./components/pages/about/about";
-// import Faq from './components/pages/faq/faq';
-import Blog from "./components/pages/blog/Blog";
-// import Careers from './components/pages/careers/careers';
-import Contact from "./components/pages/contact/contact";
-import Services from "./components/pages/services/services";
 import Footer from "./components/footer/footer";
-import TitleRoute from "./components/common/RouteTitle";
 
 class App extends React.Component {
   constructor() {
@@ -35,13 +27,11 @@ class App extends React.Component {
       <>
         {this.state.data ? (
           <div className="App">
-            {/* <InfoBar link={this.state.data.links} data={this.state.data.home} /> */}
             <NavBar
               navigation={this.state.data.navigation}
               link={this.state.data.links}
               data={this.state.data.home}
             />
-            <TitleRoute navigation={this.state.data.navigation} />
             <Routes>
               <Route
                 path="/"
